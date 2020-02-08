45d61eb6c40a58238ad6d27a87a7bcdd93d2de4b
Felipe Dalcin <felipe.dalcin@outlook.com>
2020-02-08 16:02:16 -0300

    Create and render food

--- js/main.js
+++ js/main.js
@@ -49,6 +49,21 @@ const renderRectanble = (id, x, y, width, height, fillColor, strokeCollor) => {
     context.stroke();
 }
 
+const renderCircle = (id, x, y, fillColor, strokeCollor) => {
+    const canvas = document.querySelector(id);
+    const context = canvas.getContext('2d');
+
+    const radius = blockSize / 2;
+
+    context.beginPath();
+    context.lineWidth = 1;
+    context.fillStyle = fillColor;
+    context.strokeStyle = strokeCollor;
+    context.arc(x + radius, y + radius, radius, 0, 2 * Math.PI);
+    context.fill();
+    context.stroke();
+}
+
 const renderGameBoard = () => renderRectanble('#snake-game', 0, 0, canvasSize, canvasSize, '#f7fafc', '#718096')
 
 const renderBoundaries = ({left, top, right, bottom}) => {
@@ -60,6 +75,14 @@ const renderBoundaries = ({left, top, right, bottom}) => {
 
 const renderSnake = (snake) => {
     snake.forEach((segment, index) => renderRectanble('#snake-game', segment.x, segment.y, blockSize, blockSize, '#4c51bf', '#a3bffa'));
+}
+
+const renderFood = (food) => {
+    if (!food.visible) {
+        return food = null;
+    }
+
+    renderCircle('#snake-game', food.x, food.y, '#feB2B2', '#9b2c2c');
 }
 
 const moveSnake = (snake, currentDirection) => {
@@ -87,7 +110,43 @@ const moveSnake = (snake, currentDirection) => {
     return [snakeHead, ...snakeTail];
 }
 
+const generateRandomPoint = (min, max) => {
+    const range = Math.floor(
+        ((max - min) / blockSize) - 1
+    );
+
+    const minRange = Math.floor(min / blockSize)
+
+    return Math.floor(Math.random() * range + minRange) * blockSize;
+}
+
+const createFood = (food, boundaries, snake) => {
+    if (food && food.visible) {
+        return food;
+    }
+
+    const isOnSnake = ({x, y}, sanke) => snake.some(segment => segment.x === x && segment.y === y);
+
+    const x = generateRandomPoint(boundaries.left, boundaries.right);
+    const y = generateRandomPoint(boundaries.top, boundaries.bottom);
+    const duration = Math.floor(Math.random() * (10 - 4) + 4);
+
+    food = {x, y, visible: true};
+
+    if(isOnSnake(food, snake)) {
+        food = null;
+
+        console.log('was in snake');
+        return createFood(food, boundaries, snake);
+    }
+
+    setTimeout(() => food.visible = false, duration * 1000);
+
+    return food;
+}
+
 const startGame = () => {
+    let food;
     let currentDirection = 'LEFT';
     let isChanginDirection = false;
     let snake = [{x: 400, y: 400},];
@@ -101,8 +160,10 @@ const startGame = () => {
     let game = setInterval(() => {
         isChanginDirection = false;
         renderGameBoard();
+        food = createFood(food, boundaries, snake);
         snake = moveSnake(snake, currentDirection);
         renderBoundaries(boundaries);
+        renderFood(food);
         renderSnake(snake);
     }, 100);
 }
