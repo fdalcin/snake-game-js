7d74a231056f40316c6a62709dcefbffdd64ee8c
Felipe Dalcin <felipe.dalcin@outlook.com>
2020-02-08 12:19:11 -0300

    Render and move snake

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
+const blockSize = 16;
 const canvasSize = 800;
+
+const first = (items) => {
+    items = [...items];
+
+    if (items.length === 0) {
+        return null;
+    }
+
+    return {...items[0]};
+}
+
 
 const bootGame = () => {
     startGame();
@@ -26,12 +38,46 @@ const renderBoundaries = ({left, top, right, bottom}) => {
     renderRectanble('#snake-game', left, top, right, bottom, '#f7fafc', '#4a5568')
 }
 
+const renderSnake = (snake) => {
+    snake.forEach((segment, index) => renderRectanble('#snake-game', segment.x, segment.y, blockSize, blockSize, '#4c51bf', '#a3bffa'));
+}
+
+const moveSnake = (snake, currentDirection) => {
+    const snakeHead = first(snake);
+    const snakeTail = [...snake];
+
+    snakeTail.pop();
+
+    if (currentDirection === 'UP') {
+        snakeHead.y -= blockSize;
+    }
+
+    if (currentDirection === 'DOWN') {
+        snakeHead.y += blockSize;
+    }
+
+    if (currentDirection === 'LEFT') {
+        snakeHead.x -= blockSize;
+    }
+
+    if (currentDirection === 'RIGHT') {
+        snakeHead.x += blockSize;
+    }
+
+    return [snakeHead, ...snakeTail];
+}
+
 const startGame = () => {
+    let currentDirection = 'LEFT';
+    let snake = [{x: 400, y: 400},];
     let boundaries = {top: 0, right: canvasSize, bottom: canvasSize, left: 0};
+
 
     let game = setInterval(() => {
         renderGameBoard();
+        snake = moveSnake(snake, currentDirection);
         renderBoundaries(boundaries);
+        renderSnake(snake);
     }, 100);
 }
 
